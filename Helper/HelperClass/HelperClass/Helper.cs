f128bc9acd3c490693967501543985a4c18f2818
prashant1207 <prashant1207@gmail.com>
2014-03-16 02:56:25 +0530

    ClassIntiate

---
+++ Helper/HelperClass/HelperClass/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperClass
+{
+    public class Helper
+    {
+        public string FindStr()
+        {
+            return "Prashant Test";
+        }
+    }
+}
